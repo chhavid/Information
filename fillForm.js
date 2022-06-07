481e69c53dab901db5486db2b641a9d18c7ffd68
chhavid <99489321+chhavid@users.noreply.github.com>
2022-06-07 11:08:52 +0530

    Extracted validators in a separate file.

--- fillForm.js
+++ fillForm.js
@@ -1,5 +1,7 @@
 process.stdin.setEncoding('utf8');
 const { Form } = require('./form.js');
+const { isNameValid, isDobValid, isphnNumValid, isHobbiesValid, isAddressValid }
+  = require('./validators.js');
 
 const processInput = (input, form, query) => {
   return form.addInfo(query.name, input, query.parser, query.validator);
@@ -23,43 +25,6 @@ const read = (queries, form) => {
   process.stdin.on('end', () => {
     console.log('End');
   });
-};
-
-const validateDate = (date) => {
-  return date.length === 2 && isFinite(date);
-};
-const validateMonth = (month) => {
-  return month.length === 2 && isFinite(month);
-};
-const validateYear = (year) => {
-  return year.length === 4 && isFinite(year);
-};
-
-const isNameValid = (name) => name.length > 3;
-
-const isphnNumValid = (phoneNum) => {
-  return phoneNum.trim().length === 10 && isFinite(phoneNum.trim());
-};
-
-const isHobbiesValid = (hobbies) => {
-  if (hobbies === '\n') {
-    return false;
-  }
-  return hobbies.length > 0;
-};
-
-const isDobValid = (dob) => {
-  const dobArray = dob.trim().split('-');
-  if (dobArray.length !== 3) {
-    return false;
-  }
-  const [year, month, date] = dobArray;
-  return validateYear(year) && validateMonth(month) &&
-    validateDate(date);
-};
-
-const isAddressValid = (address) => {
-  return address;
 };
 
 const identity = (info) => info.trim();
